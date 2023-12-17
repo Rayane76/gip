0ab123cfb70acb294fdfac32d771697e7a8c97ae
Rayane <rayanehiouani.rh@gmail.com>
2023-12-17 17:37:25 +0100

    added api routes

---
+++ src/app/api/admin/uploads/addArticle/route.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import connectToDB from "../../../database";
+import { NextResponse } from "next/server";
+import Categorie from "../../../models/catgorie";
+
+
+export async function POST(req){
+
+
+    try {
+        const {art,prix,mainImg,imgs,stock,sizeStock,pointStock,cat} = await req.json();
+
+        const result = await Categorie.findOne({title: cat,});
+
+        result.articles.push({
+            
+                title: art,
+                price: prix,
+                mainImage: mainImg,
+                stock: stock,
+                sizeInStock: {
+                s: sizeStock.s,
+                m: sizeStock.m,
+                l: sizeStock.l,
+                xl: sizeStock.xl,
+                xxl: sizeStock.xxl,
+              },
+              pointureInStock: {
+                point36: pointStock.point36,
+                point37: pointStock.point37,
+                point38: pointStock.point38,
+                point39: pointStock.point39,
+                point40: pointStock.point40,
+                point41: pointStock.point41,
+                point42: pointStock.point42,
+                point43: pointStock.point43,
+                point44: pointStock.point44,
+              },
+             
+        });
+
+        result.articles.images.push(imgs);
+
+        result.save();
+        
+        return NextResponse.json({
+            success: true,
+            message: "Article Added"
+          });
+       
+      } catch (e) {
+        console.log(e);
+    
+        return NextResponse.json({
+          success: false,
+          message: "Something went wrong!",
+        });
+      }
+    }
---
+++ src/app/api/getAllCategoriesArticles/[categorie]/route.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import connectToDB from "../../../database";
+import { NextResponse } from "next/server";
+import Categorie from "../../../models/catgorie";
+
+
+export async function GET(){
+    try {
+        await connectToDB();
+        const categorie = req.query;
+        const result = await Categorie.findOne({title: categorie});
+
+        return NextResponse.json({
+            data: result.articles,
+            success: true,
+            message: "success !",
+          });
+        
+       
+      } catch (e) {
+        console.log(e);
+    
+        return NextResponse.json({
+          success: false,
+          message: "Something went wrong!",
+        });
+      }
+    
+
+
+}
