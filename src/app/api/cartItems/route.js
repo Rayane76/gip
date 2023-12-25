28d3b32cd72262db41e73eedc3cec8af9d7a9729
Rayane <rayanehiouani.rh@gmail.com>
2023-12-25 23:39:40 +0100

    try1

--- src/app/api/cartItems/route.js
+++ src/app/api/cartItems/route.js
@@ -48,6 +48,31 @@ export async function GET(req){
             }
             price = price + specific.price;
         }
+
+        // let result = await Categorie.find({
+        //     articles: {
+        //         $elemMatch : {
+        //             _id: cookie.value
+        //         }
+        //     }
+        // })
+
+        // if(result.length != 0){
+        //     let specific = result[0].articles.find(x => x._id.toString() === cookie.value);
+        //     for (let j = 0; j < sizes.length; j++){
+        //         if (sizes[j].id === specific._id.toString()){
+        //            data.push({article: specific,size:sizes[j].size})
+        //            found = found + 1;
+        //         }
+        //        }
+        //        if(found === 0){
+        //        data.push({article: specific , size: null});
+        //        }
+        //        else{
+        //            found = 0;
+        //        }
+        //        price = price + specific.price;
+        // }
         
         return NextResponse.json({
             data: data,
