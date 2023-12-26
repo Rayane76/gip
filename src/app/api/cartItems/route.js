8b01d8274150fe6ddfd4c1c0f35aff6d1b07c268
Rayane <rayanehiouani.rh@gmail.com>
2023-12-26 01:05:18 +0100

    ertr

--- src/app/api/cartItems/route.js
+++ src/app/api/cartItems/route.js
@@ -8,44 +8,47 @@ export async function GET(req){
 
     try {
         let sizes = [];
-        let array = []
         let data = [];
         let found = 0;
         let price = 0;
         await connectToDB();
         const cookieStore = cookies()
-       await Promise.all(cookieStore.getAll().map(async (cookie) => {
-            if(cookie.name.length === 24){
-               sizes.push({size:cookie.value,id: cookie.name})
-            }
+    //    await Promise.all(
+        cookieStore.getAll().map(async (cookie) => {
+            // if(cookie.name.length === 24){
+            //    sizes.push({size:cookie.value,id: cookie.name})
+            // }
             if (cookie.value.length === 24){
-                let result = await Categorie.find({
-                    articles: {
-                        $elemMatch : {
-                            _id: cookie.value
-                        }
-                    }
-                })
-        
-                if(result.length != 0){
-                    let specific = await result[0].articles.find(x => x._id.toString() === cookie.value);
-                    for (let j = 0; j < sizes.length; j++){
-                        if (sizes[j].id === specific._id.toString()){
-                           data.push({article: specific,size:sizes[j].size})
-                           found = found + 1;
-                        }
-                       }
-                       if(found === 0){
-                       data.push({article: specific , size: null});
-                       }
-                       else{
-                           found = 0;
-                       }
-                       price = price + specific.price;
+                data.push(cookie.name);
+
+                // let result = await Categorie.find({
+                //     articles: {
+                //         $elemMatch : {
+                //             _id: cookie.value
+                //         }
+                //     }
                 }
-            }
+                //)
+        
+            //     if(result.length != 0){
+            //         let specific = await result[0].articles.find(x => x._id.toString() === cookie.value);
+            //         for (let j = 0; j < sizes.length; j++){
+            //             if (sizes[j].id === specific._id.toString()){
+            //                data.push({article: specific,size:sizes[j].size})
+            //                found = found + 1;
+            //             }
+            //            }
+            //            if(found === 0){
+            //            data.push({article: specific , size: null});
+            //            }
+            //            else{
+            //                found = 0;
+            //            }
+            //            price = price + specific.price;
+            //     }
+            // }
         })
-       )
+       //)
 
         return NextResponse.json({
             data: data,
