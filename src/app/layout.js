72d850261e7788c21ba7ec15e9e6dd5bf0db4b8e
Rayane <rayanehiouani.rh@gmail.com>
2024-01-03 16:15:51 +0100

    cvbcvb

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ['latin'] })
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: 'Gip',
